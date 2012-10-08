7f687043bf5e033a7c1ef5d801e76192f28e8a17
Alexander Pitkin <peleccom@gmail.com>
2012-10-08 23:02:22 +0300

    Added custom MyList<T> and MyEnum<T>

---
+++ mychess/MyEnum.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace mychess
+{
+    public class MyEnum<T> : IEnumerator<T>
+    {
+            public T[] _values;
+
+            // Enumerators are positioned before the first element
+            // until the first MoveNext() call.
+            int position = -1;
+
+            public MyEnum(T[] list)
+            {
+               _values = list;
+            }
+
+            public bool MoveNext()
+            {
+                position++;
+                return (position < _values.Length);
+            }
+
+            public void Reset()
+            {
+                position = -1;
+            }
+
+            object IEnumerator.Current
+            {
+                get
+                {
+                    return Current;
+                }
+            }
+            void IDisposable.Dispose() { }
+            public T Current
+            {
+                get
+                {
+                    try
+                    {
+                        return _values[position];
+                    }
+                    catch (IndexOutOfRangeException)
+                    {
+                        throw new InvalidOperationException();
+                    }
+                }
+            }
+        }
+}
---
+++ mychess/MyList.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace mychess
+{
+    public class MyList<T> :IList<T>
+    {
+        private T[] _contents = new T[8];
+        private int _count;
+
+        
+        public MyList(){
+            _count = 0;
+    }
+
+        public int Count
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        public bool IsReadOnly
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                return _contents[index];
+            }
+            set
+            {
+                _contents[index] = value;
+            }
+        }
+
+        public void Add(T value)
+        {
+            if (_count < _contents.Length)
+            {
+                _contents[_count] = value;
+            }
+            else
+            {
+               // change size
+                T []tmp = new T[_contents.Length * 2];
+                for (int i = 0; i < _contents.Length; i++)
+                    tmp[i] = _contents[i];
+                tmp[_contents.Length] = value;
+                // delete old array
+                _contents = null;
+                _contents = tmp;
+            }
+            _count++;
+        }
+
+
+        public void Clear()
+        {
+            _count = 0;
+            _contents = null;
+        }
+        
+
+
+        public bool Contains(T value)
+        {
+            bool found = false;
+            for (int i = 0; i < Count; i++)
+            {
+                if (_contents[i].Equals(value))
+                {
+                    found = true;
+                    break;
+                }
+            }
+            return found;
+        }
+
+        public void CopyTo(T[] array, int index)
+        {
+            int j = index;
+            for (int i = 0; i < Count; i++)
+            {
+                array.SetValue(_contents[i], j);
+                j++;
+            }
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            // Refer to the IEnumerator documentation for an example of
+            // implementing an enumerator.
+
+            // Fix it
+            return new MyEnum<T>(_contents);
+        }
+
+         IEnumerator IEnumerable.GetEnumerator()
+ {
+ return GetEnumerator();
+ }
+
+        public int IndexOf(T value)
+        {
+            int itemIndex = -1;
+            for (int i = 0; i < Count; i++)
+            {
+                if (_contents[i].Equals(value))
+                {
+                    itemIndex = i;
+                    break;
+                }
+            }
+            return itemIndex;
+
+        }
+
+       public void Insert(int index, T value)
+       {
+        if ((_count + 1 <= _contents.Length) && (index < Count) && (index >= 0))
+        {
+            _count++;
+
+            for (int i = Count - 1; i > index; i--)
+            {
+                _contents[i] = _contents[i - 1];
+            }
+            _contents[index] = value;
+        }
+    }
+
+       public bool Remove(T value)
+       {
+           int index = IndexOf(value);
+           if (index >= 0)
+           {
+               RemoveAt(index);
+               return true;
+           }
+           return false;
+       }
+
+       public void RemoveAt(int index)
+       {
+           if ((index >= 0) && (index < Count))
+           {
+               for (int i = index; i < Count - 1; i++)
+               {
+                   _contents[i] = _contents[i + 1];
+               }
+               _count--;
+           }
+       }
+    }
+}
