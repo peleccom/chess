bb55d93464c395664034a80ebf7189ccb4f2d8d5
Alexander Pitkin <peleccom@gmail.com>
2012-11-12 19:18:22 +0300

    Added GetCastling metthod whick return castling moves list

--- mychess/King.cs
+++ mychess/King.cs
@@ -29,6 +29,40 @@ namespace mychess
             return newmoves;
         }
 
+        public MyList<Position> GetCastling()
+        {
+            MyList<Position> castling = new MyList<Position>();
+            Figure fig;
+            if (ismoved())
+                return castling;
+            Side oppside = (this.Side == Side.Black)?Side.White:Side.Black;
+            if (chessfield.isDangerPosition(oppside, Position))
+                return castling;
+            if (Side == Side.White)
+            {
+                fig = chessfield.GetFigureAt(new Position(1, 1));
+                if (fig != null && !chessfield.isDangerPosition(oppside, fig.Position) && !fig.ismoved() && chessfield.GetFigureAt(new Position(2, 1)) == null && chessfield.GetFigureAt(new Position(3, 1)) == null
+                    && chessfield.GetFigureAt(new Position(4, 1)) == null)
+                    castling.Add(fig.Position);
+                fig = chessfield.GetFigureAt(new Position(8, 1));
+                if (fig != null && !chessfield.isDangerPosition(oppside, fig.Position) && !fig.ismoved() && chessfield.GetFigureAt(new Position(7, 1)) == null && chessfield.GetFigureAt(new Position(6, 1)) == null)
+                    castling.Add(fig.Position);
+
+            }
+            else
+            {
+                fig = chessfield.GetFigureAt(new Position(1, 8));
+                if (fig != null && !chessfield.isDangerPosition(oppside, fig.Position) && !fig.ismoved() && chessfield.GetFigureAt(new Position(2, 8)) == null && chessfield.GetFigureAt(new Position(3, 8)) == null
+                    && chessfield.GetFigureAt(new Position(4, 8)) == null)
+                    castling.Add(fig.Position);
+                fig = chessfield.GetFigureAt(new Position(8, 8));
+                if (fig != null && !chessfield.isDangerPosition(oppside, fig.Position) && !fig.ismoved() && chessfield.GetFigureAt(new Position(7, 8)) == null && chessfield.GetFigureAt(new Position(6, 8)) == null)
+                    castling.Add(fig.Position);
+            }
+            return castling;
+
+        }
+
         public  MyList<Position> GetMovesWithOutChecks()
         {
             return base.GetMoves();
