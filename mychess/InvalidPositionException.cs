d361177c7e8ffe4f45030b915cb78bf8a04545f9
Alexander Pitkin <peleccom@gmail.com>
2012-11-12 22:22:57 +0300

    Added own exception class

---
+++ mychess/InvalidPositionException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace mychess
+{
+[Serializable()]
+public class InvalidPositionException : System.Exception
+{
+    public InvalidPositionException() : base() { }
+    public InvalidPositionException(string message) : base(message) { }
+    public InvalidPositionException(string message, System.Exception inner) : base(message, inner) { }
+}
+}
